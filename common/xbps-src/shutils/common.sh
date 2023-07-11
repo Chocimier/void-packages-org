ad4596f36861a09e2d01af9ef70eb287b5f518ae
Piotr Wójcik <chocimier@tlen.pl>
2023-07-11 18:23:44 +0200

    Source etc/conf only once per vopt_if call

--- common/xbps-src/shutils/common.sh
+++ common/xbps-src/shutils/common.sh
@@ -182,38 +182,36 @@ set_build_options() {
         return 0
     fi
 
-    for f in ${build_options}; do
-        # Select build options from conf
-        export XBPS_CURRENT_PKG=${pkgname}
-        pkgopts="$(
-            . $XBPS_CONFIG_FILE 2>/dev/null
-            var="XBPS_PKG_OPTIONS_${XBPS_CURRENT_PKG//[^A-Za-z0-9_]/_}"
-            echo ${!var:-${XBPS_PKG_OPTIONS}}
-        )"
-        unset XBPS_CURRENT_PKG
-
-        # If pkg options were set in config(s), merge them with command line
-        if [ -n "$XBPS_ARG_PKG_OPTIONS" ]; then
-            if [ -n "$pkgopts" ]; then
-                pkgopts+=",$XBPS_ARG_PKG_OPTIONS"
-            else
-                pkgopts="$XBPS_ARG_PKG_OPTIONS"
-            fi
-        fi
-
-        OIFS="$IFS"; IFS=','
-        for j in ${pkgopts}; do
-            case "$j" in
-                "$f") options[$j]=1 ;;
-                "~$f") options[${j#\~}]=0 ;;
-            esac
-        done
-        IFS="$OIFS"
-    done
-
     for f in ${build_options_default}; do
-        [[ -z "${options[$f]}" ]] && options[$f]=1
+        options[$f]=1
     done
+
+    # Select build options from conf
+    export XBPS_CURRENT_PKG=${pkgname}
+    pkgopts="$(
+        . $XBPS_CONFIG_FILE 2>/dev/null
+        var="XBPS_PKG_OPTIONS_${XBPS_CURRENT_PKG//[^A-Za-z0-9_]/_}"
+        echo ${!var:-${XBPS_PKG_OPTIONS}}
+    )"
+    unset XBPS_CURRENT_PKG
+
+    # If pkg options were set in config(s), merge them with command line
+    if [ -n "$XBPS_ARG_PKG_OPTIONS" ]; then
+        if [ -n "$pkgopts" ]; then
+            pkgopts+=",$XBPS_ARG_PKG_OPTIONS"
+        else
+            pkgopts="$XBPS_ARG_PKG_OPTIONS"
+        fi
+    fi
+
+    OIFS="$IFS"; IFS=','
+    for j in ${pkgopts}; do
+        case "$j" in
+            "~"*) options[${j#\~}]=0 ;;
+            *) options[$j]=1 ;;
+        esac
+    done
+    IFS="$OIFS"
 
     # Sort pkg build options alphabetically.
     export PKG_BUILD_OPTIONS=" $(
